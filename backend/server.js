6ce3ff6bed54c0f4e597c8955e2cba917e9c9cac
vishalmishradev <vishalmishra@medianv.com>
2026-02-23 12:29:12 +0530

    added frontend fir the users

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 1234;
+
+app.use(cors({ origin: "*", credentials: true }));
 
 app.get("/", (req, res) => {
   res.send("Welcome to my app");
@@ -13,26 +16,31 @@ app.get("/api/users", (req, res) => {
         id: 1,
         name: "Vishal",
         email: "vishal@email.com",
+        phone: "1234569870",
       },
       {
         id: 2,
         name: "Aditi",
         email: "aditi@email.com",
+        phone: "1234569870",
       },
       {
         id: 3,
         name: "Anjali",
         email: "anjali@email.com",
+        phone: "1234569870",
       },
       {
         id: 4,
         name: "Puja",
         email: "puja@email.com",
+        phone: "1234569870",
       },
       {
         id: 5,
         name: "Aditya",
         email: "aditya@email.com",
+        phone: "1234569870",
       },
     ];
     return res.status(200).json({ success: true, user });
---
+++ frontend/script.js
@@ -0,0 +1,30 @@
+async function loadUsers() {
+  const tableBody = document.querySelector("tbody");
+
+  try {
+    const response = await axios.get("http://localhost:1234/api/users");
+
+    const users = response.data.user;
+    console.log(users);
+
+    tableBody.innerHTML = "";
+
+    users.forEach((user) => {
+      const row = `
+                <tr>
+                    <td>${user.id}</td>
+                    <td>${user.name}</td>
+                    <td>${user.email}</td>
+                    <td>${user.phone}</td>
+                </tr>
+            `;
+
+      tableBody.innerHTML += row;
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    tableBody.innerHTML = `<tr><td colspan="4" style="color: red;">Error loading users.</td></tr>`;
+  }
+}
+
+window.addEventListener("DOMContentLoaded", loadUsers);
